83a3dcbbc82458fa2d5621a195b2c67ced81cbc1
harshitaneelisetty <90179226+harshitaneelisetty@users.noreply.github.com>
2024-03-15 15:30:41 +0530

    Create numberswap.java

---
+++ numberswap.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class swap_nums {
+    public static void main(String[] args) {
+        int a = 154;
+        int b = 215;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("\nAfter swapping :");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
